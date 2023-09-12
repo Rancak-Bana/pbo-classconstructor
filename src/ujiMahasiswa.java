70c8062b55be784e0ecea992c3869067bf81f85b
Jehian <55863992+jehianth@users.noreply.github.com>
2023-09-12 12:08:08 +0700

    Update ujiMahasiswa.java

--- src/ujiMahasiswa.java
+++ src/ujiMahasiswa.java
@@ -2,8 +2,9 @@ public class ujiMahasiswa {
 
     public static void main(String[] args) {
         Mahasiswa mhs1 = new Mahasiswa("Daplun", "H1L009002");
-        mhs1.cetakInformasi();
+        //mhs1.cetakInformasi();
         Mahasiswa mhs2 = new Mahasiswa("Daplun","H1L009002", "Purwokerto");
-        System.out.println(mhs2.getNama);
+        //System.out.println(mhs2.getNama);
+        mhs2.cetakInformasi();
     }
 }
